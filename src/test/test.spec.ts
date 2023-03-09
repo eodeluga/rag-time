9ba2dd725c39714f5dcbb089df773b5e71ea8a8f
Eugene Odeluga <eodeluga@users.noreply.github.com>
2023-03-09 01:55:30 +0000

    Modified config and moved files

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 
 describe('Test template', function () {
     it('has a test folder', function () {
-        const fileExists = fs.existsSync('test/');
+        const fileExists = fs.existsSync('src/test');
         assert.expect(fileExists).be.true;
     })
 })
