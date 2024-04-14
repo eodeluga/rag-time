48be666ca488c2fe1e736c22481d4790dc51fa97
Eugene Odeluga <eodeluga@users.noreply.github.com>
2024-04-14 21:49:47 +0100

    Migrated to bun test runner

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,2 @@
-const greet: string[] = ['Hello', 'world!']
-console.log(greet[0], ' ', greet[1])
+const greet = process.env.WHO_TO_GREET
+console.log(`Hello ${greet ?? 'world'}!`)
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,18 +1,14 @@
-import { describe } from 'mocha'
-import chai from 'chai'
-import assert, { expect } from 'chai'
-import assertArrays from 'chai-arrays'
+import { describe, it, expect } from 'bun:test'
 import * as fs from 'fs'
 
 describe('Test template', function() {
   it('has a test folder', function() {
     const fileExists = fs.existsSync('./test')
-    assert.expect(fileExists).be.true
+    expect(fileExists).toBe(true)
   })
   
   it('tests an array', function() {
-    chai.use(assertArrays)
     const strArr = ['the', 'dog', 'barked']
-    expect(strArr).to.be.containingAllOf(['the', 'dog', 'barked'])
+    expect(strArr).toEqual(expect.arrayContaining(['the', 'dog', 'barked']))
   })
 })
