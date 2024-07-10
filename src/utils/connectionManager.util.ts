f9d139a24407813256cd653aff1d2612c44ff6c3
Eugene Odeluga <eodeluga@users.noreply.github.com>
2024-07-10 05:53:28 +0100

    Working on inserting into vector db

---
+++ src/utils/connectionManager.util.ts
@@ -0,0 +1,19 @@
+import { QdrantClient } from '@qdrant/qdrant-js'
+
+class ConnectionManager {
+  private static qdrantClient: QdrantClient | null = null
+  
+  private static async getQdrant() {
+    const { QDRANT_URL } = process.env
+    return new QdrantClient({url: QDRANT_URL ?? 'http://localhost:6333'})
+  }
+  
+  public static async getQdrantClient() {
+    ConnectionManager.qdrantClient = ConnectionManager.qdrantClient !== null
+      ? ConnectionManager.qdrantClient
+      : await this.getQdrant()
+    return ConnectionManager.qdrantClient
+  }  
+}
+
+export const QdrantDbConnection = await ConnectionManager.getQdrantClient()
