02f4dd9e66e6169e9f67e928e844a1c36af0ff13
Eugene Odeluga <eodeluga@users.noreply.github.com>
2024-07-11 00:55:03 +0100

    Updated inserting into vector db

--- src/services/documentIndex.service.ts
+++ src/services/documentIndex.service.ts
@@ -1,5 +1,4 @@
 import { nanoid } from 'nanoid'
-import type { DocumentChunk } from '@@models/DocumentChunk'
 import type { VectorChunk } from '@@models/VectorChunk'
 import { QdrantDbConnection } from '@@utils/connectionManager.util'
 
@@ -7,20 +6,25 @@ export class DocumentIndexService {
   private client = QdrantDbConnection
   private collectionId = nanoid()
     
-  async indexDocumentChunks(chunks: VectorChunk[]) {
-    const vectors = chunks.map((chunk) => chunk.embeddings)
-    const ids = chunks.map((chunk) => chunk.index)
-    await this.client.upsert(this.collectionId, vectors, ids)
-    /* {
-      
-      // vectors, 
-      // ids,
-    } */
+  async updateDocumentVectorChunks(chunks: VectorChunk[]) {
+    const qdrantPoints = chunks.map((chunk) => ({
+      id: chunk.index,
+      vector: [...chunk.textEmbeddings, ...chunk.summaryEmbeddings],
+      payload: {
+        index: chunk.index,
+        text: chunk.text,
+        summary: chunk.summary,
+      },
+    }))
+    
+    await this.client.upsert(this.collectionId, {
+      points: qdrantPoints,
+    })
   }
   
-  public static async searchDocumentChunks(query: string, k: number) {
-    const qdrant = await this.getQdrantClient()
-    const { ids } = await qdrant.search({query, k})
-    return ids
-  }
+  // public static async searchDocumentChunks(query: string, k: number) {
+  //   const qdrant = await this.getQdrantClient()
+  //   const { ids } = await qdrant.search({query, k})
+  //   return ids
+  // }
 }
