436ea0cb64b556a42fd5faa383fd6c91ee4a89fd
Eugene Odeluga <no@reply.com>
2024-07-10 03:51:11 +0100

    Tidied up linting

--- src/utils/textChunker.ts
+++ src/utils/textChunker.ts
@@ -1,44 +1,44 @@
-import OpenAI from 'openai';
-import type { DocumentChunk } from '@@models/DocumentChunk';
-import type { ChatCompletionTool } from 'openai/resources/index.mjs';
-import { SuccessTextChunkerResponseSchema } from 'schemas/textChunker.schema';
+import OpenAI from 'openai'
+import type { DocumentChunk } from '@@models/DocumentChunk'
+import type { ChatCompletionTool } from 'openai/resources/index.mjs'
+import { SuccessTextChunkerResponseSchema } from 'schemas/textChunker.schema'
 
 const textChunkFunction = {
-    type: "function",
-    function: {
-      name: "text_chunker",
-      description: "Process text into chunks for RAG purposes",
-      parameters: {
-        type: "object",
-        properties: {
-          chunks: {
-            type: "array",
-            items: {
-              type: "object",
-              properties: {
-                index: {
-                  type: "integer",
-                  description: "The index of the chunk"
-                },
-                text: {
-                  type: "string",
-                  description: "The text of the chunk"
-                },
-                summary: {
-                  type: "string",
-                  description: "Keywords used in `text` property"
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  } satisfies ChatCompletionTool
+  type: 'function',
+  function: {
+    name: 'text_chunker',
+    description: 'Process text into chunks for RAG purposes',
+    parameters: {
+      type: 'object',
+      properties: {
+        chunks: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              index: {
+                type: 'integer',
+                description: 'The index of the chunk',
+              },
+              text: {
+                type: 'string',
+                description: 'The text of the chunk',
+              },
+              summary: {
+                type: 'string',
+                description: 'Keywords used in `text` property',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+} satisfies ChatCompletionTool
 
 async function chunkTextWithLlm(text: string) {
   const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY,
   })
   
   const prompt = ((text: string) => (
@@ -50,11 +50,11 @@ async function chunkTextWithLlm(text: string) {
     model: 'gpt-3.5-turbo',
     response_format: { type: 'json_object' },
     n: 1,
-    tools: [ textChunkFunction ],
+    tools: [textChunkFunction],
     tool_choice: 'auto',
     messages: [
       { role: 'system', content: 'You process text into chunks for RAG purposes' },
-      { role: 'user', content: prompt(text) }
+      { role: 'user', content: prompt(text) },
     ],
   })
   
