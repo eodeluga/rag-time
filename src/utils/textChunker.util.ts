bd506ba97a152be55a53ff20cbe879b0eae4a10a
Eugene Odeluga <no@reply.com>
2024-07-13 04:07:18 +0100

    Testing langchain text splitting

--- src/utils/textChunker.util.ts
+++ src/utils/textChunker.util.ts
@@ -1,52 +1,31 @@
 import OpenAI from 'openai'
 import type { DocumentChunk } from '@@models/DocumentChunk'
-import type { ChatCompletionTool } from 'openai/resources/index.mjs'
 import { TextChunkerResponseValidator } from '@@validators/textChunker.validator'
-
-const textChunkFunction = {
-  type: 'function',
-  function: {
-    name: 'text_chunker',
-    description: 'Process text into chunks for RAG purposes',
-    parameters: {
-      type: 'object',
-      properties: {
-        chunks: {
-          type: 'array',
-          items: {
-            type: 'object',
-            properties: {
-              index: {
-                type: 'integer',
-                description: 'The index of the chunk',
-              },
-              text: {
-                type: 'string',
-                description: 'The text of the chunk',
-              },
-              summary: {
-                type: 'string',
-                description: 'Keywords used in `text` property',
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-} satisfies ChatCompletionTool
+import { textChunkFunction } from '@@functions/textChunk.function'
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters'
 
 const createPrompt = ((text: string) => (
-  `${text}\n\n` +
-  'Process the above text into a series of dynamically sized overlapping chunks and return a JSON array as the result'
+  `Text to chunk: ${text}\n\n` +
+  'Strictly follow this instruction: ' +
+  'Split the \'Text to chunk\' into overlapping chunks in recursive pattern then return strictly adhering as JSON array'
 ))
 
-const normaliseText = ((text: string) => text.replace(/[\n.,/#!$%^&*;:{}=\-_`~()]/g, '').toLowerCase())
+const normaliseText = ((text: string) => text
+  .replace(/[\n.,/#!$%^&*;:{}=\-_`~()]/g, ' ')
+  .toLowerCase()
+  .replaceAll(/\s+/g, ' '))
 
 async function chunkTextWithLlm(text: string) {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   })
+  
+  // Split text into sentences on full stops but not if sentence is 
+  const recursiveCharacterSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 50,
+    chunkOverlap: 25,
+  })
+  const chunks = await recursiveCharacterSplitter.splitText(normaliseText(text))
   
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
