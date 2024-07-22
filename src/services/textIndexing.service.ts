1c4418e3b8f7ecdd3797a3ca60ae86c33edb81bb
Eugene Odeluga <eodeluga@users.noreply.github.com>
2024-07-22 05:59:14 +0100

    Added local embedding and tests

--- src/services/textIndexing.service.ts
+++ src/services/textIndexing.service.ts
@@ -3,6 +3,7 @@ import { QdrantDbConnection } from '@@utils/connectionManager.util'
 import type { TextEmbedding } from '@@models/TextEmbedding'
 import type OpenAI from 'openai'
 import { TextEmbeddingService } from './textEmbedding.service'
+import { encode } from 'gpt-3-encoder'
 
 export class TextIndexingService {
   private client = QdrantDbConnection
@@ -45,6 +46,50 @@ export class TextIndexingService {
       status: result.status,
     }
   }
+
+  async gpt3InsertIndex(textEmbeddingArr: TextEmbedding[]) {
+    const largestEmbeddingSize = textEmbeddingArr.map((point) => point.embedding.length).sort((a, b) => b - a)[0]
+    
+    const padArray = (arr: number[], size: number) => {
+      if (arr.length >= size) {
+        return arr
+      }
+      const pad = arr.concat(Array(size - arr.length).fill(0))
+      return pad
+    }
+    
+    const qdrantPoints = textEmbeddingArr.map((textEmbedding) => ({
+      id: textEmbedding.index,
+      vector: textEmbedding.embedding.length < largestEmbeddingSize
+        ? padArray(textEmbedding.embedding, largestEmbeddingSize)
+        : textEmbedding.embedding,
+      payload: {
+        index: textEmbedding.index,
+        text: textEmbedding.text,
+      },
+    }))
+    
+    
+    await this.client.recreateCollection(this.collectionId, {
+      vectors: {
+        size: qdrantPoints[0].vector.length,
+        distance: 'Cosine',
+      },
+      optimizers_config: {
+        default_segment_number: 2,
+      },
+      replication_factor: 2,
+    })
+    
+    const result = await this.client.upsert(this.collectionId, {
+      points: qdrantPoints,
+    })
+    
+    return {
+      collectionId: this.collectionId,
+      status: result.status,
+    }
+  }
   
   async searchIndex(query: string, limit: number, openai: OpenAI) {
     const textEmbeddingService = new TextEmbeddingService(openai)
@@ -52,6 +97,16 @@ export class TextIndexingService {
     
     const results = await this.client.search(this.collectionId, {
       vector: queryEmbedding[0].embedding,
+      limit,
+    })
+    
+    return results.map((result) => (result.payload ? result.payload.text : []))
+  }
+  
+  async gpt3SearchIndex(query: string, limit: number) {
+    const queryEmbedding = encode(query)    
+    const results = await this.client.search(this.collectionId, {
+      vector: queryEmbedding,
       limit,
     })
     
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,14 +1,54 @@
-import { describe, it, expect } from 'bun:test'
-import * as fs from 'fs'
+import { TextEmbeddingService } from '@@services/textEmbedding.service'
+import { LlmTextSplitters } from '@@utils/llmTextSplitters.util'
+import { describe, it } from 'bun:test'
+// import { describe, it } from 'mocha'
 
-describe('Test template', function() {
-  it('has a test folder', function() {
-    const fileExists = fs.existsSync('./test')
-    expect(fileExists).toBe(true)
+import OpenAI from 'openai'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+// Chunk the first page of Harry Potter and the Philosopher's Stone
+const text = 'Mr . and Mrs . Dursley, of number four, Privet Drive, '
+  + 'were proud to say that they were perfectly normal, thank you very much.'
+  + 'They were the last people you’d expect to be involved in anything strange or mysterious, '
+  + 'because they just didn’t hold with such nonsense.'
+  + 'Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beef'
+
+describe('Test template', async function() {
+  it('tests the performance of gpt-3-encoder library', async function() {
+    const llmTextSplitters = new LlmTextSplitters(openai)
+    const sentences = await llmTextSplitters.textChunker(text)
+    const textEmbeddingService = new TextEmbeddingService(openai) 
+    const startTime = performance.now()
+    const embedding = await textEmbeddingService.gpt3EmbedSentences(sentences)
+    const endTime = performance.now()
+    const timeTaken = endTime - startTime
+    console.log(`Time taken to produce embedding of size ${embedding.length} gpt-3-encoder: ${timeTaken} ms`)
   })
   
-  it('tests an array', function() {
-    const strArr = ['the', 'dog', 'barked']
-    expect(strArr).toEqual(expect.arrayContaining(['the', 'dog', 'barked']))
+  it('tests the performance of Open AI embedding', async function() {
+    const llmTextSplitters = new LlmTextSplitters(openai)
+    const sentences = await llmTextSplitters.textChunker(text)
+    const textEmbeddingService = new TextEmbeddingService(openai) 
+    const startTime = performance.now()
+    const embedding = await textEmbeddingService.embedSentences(sentences)
+    const endTime = performance.now()
+    const timeTaken = endTime - startTime
+    console.log(`Time taken to produce embedding of size ${embedding.length} Open AI: ${timeTaken} ms`)
   })
+  
+  it('tests the performance of transformers library', async function() {
+    const llmTextSplitters = new LlmTextSplitters(openai)
+    const sentences = await llmTextSplitters.textChunker(text)
+    const textEmbeddingService = new TextEmbeddingService(openai) 
+    const startTime = performance.now()
+    await textEmbeddingService.transformersEmbedSentences(sentences)
+    const endTime = performance.now()
+    const timeTaken = endTime - startTime
+    console.log(`Time taken to produce embedding of size ${sentences.length} transformers: ${timeTaken} ms`)
+  })
+  
 })
