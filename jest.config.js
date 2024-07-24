a9d1889731e6e0698b8dff4b2c586b09913dbe86
Eugene Odeluga <eodeluga@users.noreply.github.com>
2024-07-24 06:58:24 +0100

    Adding jest test runner

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }]
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
+}
