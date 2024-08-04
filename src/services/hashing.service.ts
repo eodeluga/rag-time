7687f0ea79cefb56463febb9fd7fb202ee118a58
Eugene Odeluga <eodeluga@users.noreply.github.com>
2024-08-04 01:44:12 +0100

    Changed hashing util to service

---
+++ src/services/hashing.service.ts
@@ -0,0 +1,24 @@
+import { createHash } from 'crypto'
+import { QdrantDbConnection } from '@@utils/connectionManager.util'
+export class HashingService {
+  
+  async hashExists(hash: string) {
+    try {
+      const qdrant = await QdrantDbConnection.getQdrantClient()
+      const { exists } = await qdrant.collectionExists(hash)
+      return exists
+    } catch (error) {
+      throw new Error(`Error checking if hash exists: ${error instanceof Error ? error.message : ''}`)
+    }
+  }
+  
+  async hashBuffer(fileBuffer: Buffer): Promise<string> {
+    try {
+      const hash = createHash('sha256')
+      hash.update(fileBuffer)
+      return hash.digest('hex')
+    } catch (error) {
+      throw new Error(`Error creating hash: ${error instanceof Error ? error.message : ''}`)
+    }
+  }
+}
