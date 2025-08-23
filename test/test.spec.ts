add56ea6f6970f63a8f61452d032668eb34c57ea
Eugene Odeluga <eodeluga@users.noreply.github.com>
2025-08-23 19:45:06 +0100

    Updated tests to use bun test runner

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+import { it, describe, expect } from 'bun:test'
 
 describe('Test template', function() {
   it('has a test folder', function() {
@@ -7,7 +8,7 @@ describe('Test template', function() {
   })
   
   it('tests an array', function() {
-    const strArr = ['the', 'dog', 'barked']
-    expect(strArr).toEqual(expect.arrayContaining(['the', 'dog', 'barked']))
+    const strArr = ['the', 'dog', 'barked'] as const
+    expect(strArr).toEqual(['the', 'dog', 'barked'])
   })
 })
